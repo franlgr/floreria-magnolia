3abc0be28e6ad1b62de4649968a2de85dbf93257
franlgr <105466301+franlgr@users.noreply.github.com>
2024-12-23 19:09:54 -0300

    asd

--- server.js
+++ server.js
@@ -12,22 +12,6 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
-// Use 0.0.0.0 to allow access from other devices on the network
-app.listen(PORT, '0.0.0.0', () => {
+app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
-  console.log(`Access it on your local network at http://${getLocalIp()}:${PORT}`);
 });
-
-// Helper function to get the local IP address
-function getLocalIp() {
-  const os = require('os');
-  const interfaces = os.networkInterfaces();
-  for (const name of Object.keys(interfaces)) {
-    for (const net of interfaces[name]) {
-      if (net.family === 'IPv4' && !net.internal) {
-        return net.address;
-      }
-    }
-  }
-  return 'localhost';
-}
