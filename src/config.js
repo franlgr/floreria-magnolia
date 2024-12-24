915f33ded6e3f7ed19beff3e5dc696fb0e5ecf7f
franlgr <105466301+franlgr@users.noreply.github.com>
2024-12-24 00:34:54 -0300

    asd

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 import { config } from "dotenv";
 config();
 
-export const MERCADOPAGO_API_KEY = process.env.MERCADOPAGO_API_KEY;
+export const MERCADOPAGO_API_KEY = "APP_USR-67613722-9e36-4871-a97c-0e4d66fdede8";
--- src/controllers/payment.controller.js
+++ src/controllers/payment.controller.js
@@ -1,46 +1,34 @@
-// import { MercadoPago, Payment } from "mercadopago";
-// import { MERCADOPAGO_API_KEY } from "../config.js";
-
-const { MercadoPago, Payment } = require("mercadopago");
-const { MERCADOPAGO_API_KEY } = require("../config.js");
-
-
-const client = new MercadoPago({ accessToken: MERCADOPAGO_API_KEY });
-const payments = new Payment(client);
+import mercadopage from "mercadopago";
+import { MERCADOPAGO_API_KEY } from "../config.js";
 
 export const createOrder = async (req, res) => {
+  mercadopage.configure({
+    access_token: MERCADOPAGO_API_KEY,
+  });
+
   try {
-    const result = await payments.create({
-      body: {
-        additional_info: {
-          items: [
-            {
-              id: "item1",
-              title: "Laptop",
-              description: "A high-performance laptop",
-              picture_url: "https://example.com/laptop.png", // Cambia esto a una URL válida si tienes imágenes
-              category_id: "electronics",
-              quantity: 1,
-              unit_price: 500,
-            },
-          ],
+    const result = await mercadopage.preferences.create({
+      items: [
+        {
+          title: "Laptop",
+          unit_price: 500,
+          currency_id: "PEN",
+          quantity: 1,
         },
-        notification_url: "https://plancheto.com/webhook",
-        back_urls: {
-          success: "http://plancheto.com/success",
-          // pending: "http://plancheto.com/pending",
-          // failure: "http://plancheto.com/failure",
-        },
-      },
-      requestOptions: {
-        idempotencyKey: "unique-key-for-request",
+      ],
+      notification_url: "https://plancheto.com/webhook",
+      back_urls: {
+        success: "http://plancheto.com/success",
+        // pending: "https://e720-190-237-16-208.sa.ngrok.io/pending",
+        // failure: "https://e720-190-237-16-208.sa.ngrok.io/failure",
       },
     });
 
     console.log(result);
-    res.json(result.response); // Devuelve el cuerpo de la respuesta
+
+    // res.json({ message: "Payment creted" });
+    res.json(result.body);
   } catch (error) {
-    console.error(error);
     return res.status(500).json({ message: "Something goes wrong" });
   }
 };
@@ -50,13 +38,48 @@ export const receiveWebhook = async (req, res) => {
     const payment = req.query;
     console.log(payment);
     if (payment.type === "payment") {
-      const data = await client.get(`/v1/payments/${payment["data.id"]}`);
+      const data = await mercadopage.payment.findById(payment["data.id"]);
       console.log(data);
     }
 
     res.sendStatus(204);
   } catch (error) {
-    console.error(error);
+    console.log(error);
     return res.status(500).json({ message: "Something goes wrong" });
   }
 };
+
+
+
+
+
+
+
+
+
+//--------------------------------------------------------------------
+
+
+// const client = new MercadoPago({ accessToken: '<ACCESS_TOKEN>' });
+// const payments = new Payment(client);
+
+// payments.create({
+//   body: {
+//     additional_info: {
+//       items: [
+//         {
+//           id: 'MLB2907679857',
+//           title: 'Point Mini',
+//           description: 'Point product for card payments via Bluetooth.',
+//           picture_url: 'https://http2.mlstatic.com/resources/frontend/statics/growth-sellers-landings/device-mlb-point-i_medium2x.png',
+//           category_id: 'electronics',
+//           quantity: 1,
+//           unit_price: 58,
+//         }
+//       ],
+//     },
+//   requestOptions: { idempotencyKey: '<SOME_UNIQUE_VALUE>' }
+//   }
+// })
+//   .then((result) => console.log(result))
+//   .catch((error) => console.log(error));
