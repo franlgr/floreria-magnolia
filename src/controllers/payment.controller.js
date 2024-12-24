0c7ddfc6da927adba042ee077a56324d23df4f54
franlgr <105466301+franlgr@users.noreply.github.com>
2024-12-24 00:14:03 -0300

    asd

--- src/controllers/payment.controller.js
+++ src/controllers/payment.controller.js
@@ -6,7 +6,7 @@ import mercadopago from "mercadopago";
 import { MERCADOPAGO_API_KEY } from "../config.js";
 
 export const createOrder = async (req, res) => {
-  mercadopage.configure({
+  mercadopago.configure({
     access_token: MERCADOPAGO_API_KEY,
   });
 
